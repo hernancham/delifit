f05b4c95b5ee3056b0b1753f0be9f742aef47b5d
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-08-12 22:04:08 -0500

    chore: Update npm dependencies to include next-themes 0.3.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// Componentes de Contexto
+import { ThemeProvider } from "@/components/context/ThemeProvider";
+
+// Fuentes de Google
+import { Inter as FontSans } from "next/font/google";
+
+// Utilidades
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +26,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='es'>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
