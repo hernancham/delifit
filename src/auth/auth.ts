1a2b0e4e3a497788459dce8d3103172205144422
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-08-17 01:03:16 -0500

    feat: Update NextAuth configuration and routes in auth.ts

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,13 +2,24 @@ import NextAuth from "next-auth";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
-import authConfig from "@/auth/auth.config";
+import { apiAuthRoute, logoutRoute, registerRoute, loginRoute } from "./routes";
+import { authConfig } from "./auth.config";
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
+  basePath: apiAuthRoute,
   adapter: PrismaAdapter(prisma),
-  ...authConfig,
-  session: { strategy: "jwt" },
+  pages: {
+    signIn: loginRoute,
+    signOut: logoutRoute,
+    newUser: registerRoute,
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, //30 days
+    updateAge: 24 * 60 * 60, //24 hours
+  },
   callbacks: {
     // jwt() se ejecuta cada vez que se crea o actualiza un token JWT.
     // Aquí es donde puedes agregar información adicional al token.
@@ -38,7 +49,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       });
     },
   },
-  pages: {
-    signIn: "/login",
-  },
+  ...authConfig,
 });
