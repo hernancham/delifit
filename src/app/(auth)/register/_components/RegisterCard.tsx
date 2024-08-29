632076bb88ad1e4fd0b6191e1e9e70df2617b362
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-08-29 01:50:21 -0500

    feat: Actualizar página de registro y corregir estilos

--- src/app/(auth)/register/_components/RegisterCard.tsx
+++ src/app/(auth)/register/_components/RegisterCard.tsx
@@ -11,47 +11,45 @@ import {
 } from "@/components/ui/card";
 import { RegisterForm } from "./RegisterForm";
 import { loginRoute } from "@/auth/routes";
-import { JSX } from "react";
 
 export const RegisterCard = () => {
   return (
-    <div className='min-h-screen text-mcd-black lg:grid lg:grid-cols-[3fr_2fr] lg:gap-16'>
-      {/* Imagen visible solo en pantallas grandes */}
-      <div className='relative hidden lg:flex lg:items-center lg:justify-center'>
-        <div className="bg-[url('/media/img1_register.jpg')] bg-center bg-cover w-[90%] h-[90vh] rounded-lg border border-gray-300 overflow-hidden"></div>
-      </div>
-
-      <main className='flex items-center justify-center p-8 lg:p-0'>
-        <div className='flex flex-col items-center justify-center max-lg:mt-28'>
-          <section className='w-full max-w-md'>
-            <Card className='w-[400px] shadow-md'>
-              <CardHeader>
-                <div className="bg-[url('/assets/delifit_logo.svg')] bg-center bg-cover w-20 h-20 mx-auto mb-4"></div>
-                <CardTitle className='text-3xl font-semibold text-center'>
-                  Registrar cuenta
-                </CardTitle>
-                <CardDescription className='text-center'>
-                  Crea una cuenta para empezar a disfrutar de nuestros servicios
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <RegisterForm />
-              </CardContent>
-              <CardFooter className='flex justify-center'>
-                <div className='text-sm text-center'>
-                  ¿Ya tienes una cuenta?{" "}
-                  <Link
-                    href={loginRoute}
-                    className='underline'
-                  >
-                    Iniciar sesión
-                  </Link>
-                </div>
-              </CardFooter>
-            </Card>
-          </section>
+    <Card className='max-w-md shadow-md'>
+      <CardHeader>
+        <Link
+          href='/'
+          className='flex items-center justify-center mx-auto mb-2 bg-lime-50 rounded-full size-28 '
+        >
+          <span className='sr-only'>Home</span>
+          <img
+            src='/assets/delifit_logo.svg'
+            alt='logo delifit'
+            width={20}
+            height={20}
+            className='size-4/5'
+          />
+        </Link>
+        <CardTitle className='text-3xl font-semibold text-center'>
+          Registrar cuenta
+        </CardTitle>
+        <CardDescription className='text-center'>
+          Crea una cuenta para empezar a disfrutar de nuestros servicios
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <RegisterForm />
+      </CardContent>
+      <CardFooter className='flex justify-center'>
+        <div className='text-sm text-center'>
+          ¿Ya tienes una cuenta?{" "}
+          <Link
+            href={loginRoute}
+            className='underline'
+          >
+            Iniciar sesión
+          </Link>
         </div>
-      </main>
-    </div>
+      </CardFooter>
+    </Card>
   );
 };
