2039faaa15e5532b749aaed1ae0f06bad6f2baa4
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-08-30 15:53:10 -0500

    feat: Update layout with container class and minimum height

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "./_components/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section>
+    <section className='container min-h-screen px-4'>
       <Navbar />
       {children}
     </section>
