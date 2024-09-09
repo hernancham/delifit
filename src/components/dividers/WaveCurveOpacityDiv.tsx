5b046738e4ecc206c819b9cf80aa089500873ef4
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-09-08 20:57:29 -0500

    feat: Agregar componentes WaveCurveOpacityDiv y WavesDiv
    Se agregaron los componentes WaveCurveOpacityDiv y WavesDiv para representar elementos de división con efectos de ondas en la parte superior o inferior de la página. Estos componentes utilizan SVG para dibujar las formas de onda y se pueden personalizar mediante las propiedades "side" y "className".
    
    [Issue #X]

---
+++ src/components/dividers/WaveCurveOpacityDiv.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/lib/utils";
+
+interface WaveCurveOpacityDivProps {
+  side?: "top" | "bottom";
+  className?: string;
+}
+
+export const WaveCurveOpacityDiv = ({
+  side = "top",
+  className,
+}: WaveCurveOpacityDivProps) => {
+  return (
+    <div
+      className={cn("absolute inset-x-0 -z-10", {
+        "top-0 rotate-0": side === "top",
+        "bottom-0 rotate-180": side === "bottom",
+      })}
+    >
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 35.28 2.17'
+        preserveAspectRatio='none'
+        className={className}
+      >
+        <path
+          d='M0 .5c3.07.55 9.27-.42 16.14 0 6.88.4 13.75.57 19.14-.11V0H0z'
+          fill='%23000000'
+        ></path>
+        <path
+          d='M0 1c3.17.8 7.29-.38 10.04-.55 2.75-.17 9.25 1.47 12.67 1.3 3.43-.17 4.65-.84 7.05-.87 2.4-.02 5.52.88 5.52.88V0H0z'
+          opacity='.5'
+          fill='%23000000'
+        ></path>
+        <path
+          d='M0 1.85c2.56-.83 7.68-.3 11.79-.42 4.1-.12 6.86-.61 9.58-.28 2.73.33 5.61 1.17 8.61 1 3-.19 4.73-.82 5.3-.84V.1H0z'
+          opacity='.5'
+          fill='%23000000'
+        ></path>
+      </svg>
+    </div>
+  );
+};
