83c0bd979b51b511e7520549aa211934b38818aa
Danilo MM <rdmoronm@unjbg.edu.pe>
2024-11-01 17:45:11 -0500

    Iconos de redes sociales (svg) convertidos a Componentes tsx

---
+++ public/social-icons/TwitterXIcon.tsx
@@ -0,0 +1,47 @@
+export const TwitterXIcon: React.FC<React.SVGProps<SVGElement>> = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='48'
+    height='48'
+    baseProfile='basic'
+    viewBox='0 0 48 48'
+  >
+    <linearGradient
+      id='U8Yg0Q5gzpRbQDBSnSCfPa'
+      x1='4.338'
+      x2='38.984'
+      y1='-10.056'
+      y2='49.954'
+      gradientUnits='userSpaceOnUse'
+    >
+      <stop
+        offset='0'
+        stopColor='#4b4b4b'
+      ></stop>
+      <stop
+        offset='0.247'
+        stopColor='#3e3e3e'
+      ></stop>
+      <stop
+        offset='0.686'
+        stopColor='#2b2b2b'
+      ></stop>
+      <stop
+        offset='1'
+        stopColor='#252525'
+      ></stop>
+    </linearGradient>
+    <path
+      fill='url(#U8Yg0Q5gzpRbQDBSnSCfPa)'
+      d='M38 42H10a4 4 0 0 1-4-4V10a4 4 0 0 1 4-4h28a4 4 0 0 1 4 4v28a4 4 0 0 1-4 4'
+    ></path>
+    <path
+      fill='#fff'
+      d='M34.257 34H27.82L13.829 14h6.437zm-5.67-1.696h2.563L19.499 15.696h-2.563z'
+    ></path>
+    <path
+      fill='#fff'
+      d='m15.866 34 7.203-8.344-.942-1.249L13.823 34zM24.45 21.721l.905 1.289L33.136 14h-2z'
+    ></path>
+  </svg>
+);
